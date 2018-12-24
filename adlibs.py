e1f83382f1ecfe680540e3e4cef2b94343c88344
Jenna <jenna.r.shannon@gmail.com>
2018-12-24 13:51:31 -0500

    adding new game

---
+++ adlibs.py
@@ -0,0 +1,33 @@
+print("Welcome to Jenna's Adlib Mania Game!")
+print(".\n.\n.\n.\n.\n.\n")
+
+print("Let's get started")
+
+propername1 = input("Please enter a proper name:\n")
+
+noun1 = input("Please enter a noun:\n")
+
+adjective1 = input("Please enter an adjective:\n")
+
+propername2 = input("Please enter a proper name:\n")
+
+verb1 = input("Please enter a verb:\n")
+
+adjective2 = input("Please enter an adjective:\n")
+
+noun2 = input("Please enter a noun:\n")
+
+
+
+
+print(" \n \n \n \n \n ")
+print("It was a very " + adjective1 + " day. \n" + propername1  
+		+ " was headed to the " + noun1 + " to pick up some packages. \n"
+		"Christmas was almost here. On the way " + 
+		propername1 + " saw " + propername2 +
+		" who said he wanted to \n" + verb1 + " later. " +
+		propername1 + " said 'Sure! Sounds great!'\n" +
+		propername1 + " continued on her way. \n" +
+		"She was feeling mighty " + adjective2)
+
+
